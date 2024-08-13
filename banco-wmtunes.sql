f13f252a18c6fb2a65527c118afc158a73231e64
Waldylene Gomes <wsg4@aluno.ifal.edu.br>
2024-08-13 15:53:51 -0300

    update and inserts

--- banco-wmtunes.sql
+++ banco-wmtunes.sql
@@ -2,7 +2,7 @@ create database wmtunes;
 use wmtunes;
 
 create table artista(
-cpf varchar(11) primary key,
+cpf varchar(14) primary key,
 bio varchar(100),
 num varchar(100),
 nome varchar(50),
@@ -17,7 +17,8 @@ valor double
 );
 
 create table usuarios(
-nome varchar(100) primary key,
+cpf varchar(14) primary key,
+nome varchar (50),
 num varchar(100),
 email varchar(50),
 FK_plano_cod varchar (50),
@@ -35,9 +36,7 @@ create table musica(
 nome varchar(78) primary key,
 letra varchar(1000),
 lancamento int,
-descricao varchar(1000),
-FK_usuarios_nome varchar(100),
-foreign key (FK_usuarios_nome) references usuarios(nome)
+descricao varchar(1000)
 );
 
 create table genero_musical(
@@ -46,7 +45,7 @@ nome varchar(50)
 );
 
 create table artistausuarios(
-FK_artista_cpf varchar(11),
+FK_artista_cpf varchar(14),
 FK_usuarios_nome varchar(100),
 primary key(FK_artista_cpf, FK_usuarios_nome),
 foreign key(FK_artista_cpf) references artista (cpf),
@@ -63,11 +62,37 @@ foreign key(FK_usuarios_cod) references playlist (cod)
 
 create table generoartista(
 FK_genero_cod varchar(50),
-FK_artista_cpf varchar(11),
+FK_artista_cpf varchar(14),
 primary key(FK_genero_cod, FK_artista_cpf),
 foreign key(FK_genero_cod) references genero (cod),
 foreign key(FK_artista_cpf) references artista (cpf) 
 );
+
+create table musicagenero(
+FK_musica_nome varchar(78),
+FK_genero_cod varchar (50),
+primary key(FK_musica_nome, FK_genero_cod),
+foreign key(FK_musica_nome) references musica (nome),
+foreign key(FK_genero_cod) references genero(cod)
+);
+
+create table musicaartista(
+FK_musica_nome varchar (78),
+FK_artista_cpf varchar (14),
+primary key(FK_musica_nome, FK_artista_cpf),
+foreign key(FK_musica_nome) references musica (nome),
+foreign key(FK_artista_cpf) references artista (cpf)
+);
+
+create table usuariomusica(
+FK_usuario_cpf varchar (14),
+FK_musica_nome varchar(78),
+primary key(FK_usuario_cpf, FK_musica_nome),
+foreign key(FK_usuario_cpf) references usuario (cpf),
+foreign key(FK_musica_nome) references musica (nome)
+);
+
+
 
 
 
---
+++ insert.WMtunes.sql
@@ -0,0 +1,10 @@
+insert into artista (cpf, bio, num, nome, email) values 
+("000.000.000-00", "Cantora Gospel", "(82)99370-1670", "Laura Cristine", "laurinha@gmail.com"),
+("111.111.111-11", "Comunidade católica", "(82)99450-2690", "Mariana Vanessa", "marianavanessa@gmail.com"),
+("222.222.222-22", "🎧", "82)99230-6609", "Waldylene Gomes", "Waldyleneggomes@gaail.com");
+
+insert into plano (cod, tipo, descrição, valor) values 
+("222485", "Mensal", "o plano Premium Individual, com direito a apenas 1 conta", 19.50),
+("", "Comunidade catlica", "(82)99450-2690", "Mariana Vanessa", "marianavanessa@gmail.com"),
+("222.222.222-22", "🎧", "82)99230-6609", "Waldylene Gomes", "Waldyleneggomes@gaail.com");
+
