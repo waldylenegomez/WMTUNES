7594b0783f9ec0b0e5c63c0096613a6d91de3ced
Mariana-Vanessa <mvs38@aluno.ifal.edu.br>
2025-01-24 17:22:09 -0300

    Adiconando tabelas

--- Sequelizer-WM/sequelizer-0/App.js
+++ Sequelizer-WM/sequelizer-0/App.js
@@ -18,13 +18,49 @@ const relacionamentos = require('./models/relacionamentos')
   
       // Criar um usuário
       const artista1 = await Artista.create({
-          cpf: '12131',
-          bio: 'adwq', 
-          num: '210-34', 
-          nome: 'dkopwpd', 
-          email: 'sjdfjdqwo', 
-          foto: 'sfjkdqopwd'
-        });
+          cpf: '000.000.000-00',
+          bio: 'Vanessa da Mata, MPB,', 
+          num: '(95)002-8922', 
+          nome: 'Vanessa da Mata', 
+          email: 'agenciadamata@gmail.com', 
+          foto: '/img'
+        })
+        
+        const usuario1 = await Usuarios.create({
+            cpf: '111.111.111-11',
+            nome: 'Mariana Vanessa', 
+            num: '(82)122-4567', 
+            email: 'marizinha@gmail.com', 
+            foto: '/img',
+            biografia: 'Blessed <3',
+            link: 'instagram.com',
+            pronomes: 'Ela/Dela',
+      });
+
+      const musica1 = await Musica.create({
+        nome: 'Boa sorte', 
+        letra: 'É só isso. Não tem mais jeito. Acabou. Boa sorte',
+        lancamento: '2011', 
+        descricao: 'Ouça Vanessa da Mata: https://SMB.lnk.to/VanessadaMata',
+  });
+
+    const playlist1 = await Playlist.create({
+    cod: '222485', 
+    nome: 'Músicas para relaxar',
+});
+
+    const generomusicall = await Genero_musical.create({
+    cod: '123456', 
+    nome: 'MPB',
+});
+
+    const plano1 = await Plano.create({
+    cod: '678912', 
+    tipo: 'Mensal',
+    descricao: 'Plano Premium Individual, 1 conta Premium' ,
+    valor: '19.50',
+
+});
 
     } catch (error) {
       console.error('Erro ao sincronizar o banco de dados:', error);
